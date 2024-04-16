cc5bad78c02f2304fb059913fad5f68ce0f3f5d5
lucasens1 <lucasensini1@gmail.com>
2024-04-16 18:06:54 +0200

    fixed bugs with for .. in

--- le-12-fatiche-di-javascript/app.js
+++ le-12-fatiche-di-javascript/app.js
@@ -5,14 +5,14 @@ const array = [];
 
 
 //All'input viene aggiunto l'EventListener
-input.addEventListener('keypress', function(event) {
+input.addEventListener('keypress', function (event) {
 
     /*
         L'event in questo caso controlla nel primo caso se quello che inserisco
         è diverso dal valore del tasto Enter ossia 'Enter', e se l'input -> valure.lenght è 0
         esce dall'EventListener
     */
-    if (event.code != 'Enter')    return;
+    if (event.code != 'Enter') return;
     //Tolgo la length e controllo se è vuota la stringa
     if (input.value.length === 0) return;
 
@@ -40,21 +40,21 @@ input.addEventListener('keypress', function(event) {
     console.log(array)
 
     //Counter è inizializzato come stringa e poi incrementato ne cambio dunque il valore
-    let counter = 0;
+    /* let counter = 0; */
     //Item viene inizializzato alla prima posizione dell'array 
-    let item = array[0];
+    /* let item = array[0]; */
     //Max?
-    let max = 1;
+    /* let max = 1; */
     //Oggetto e non un array
     const elems = {};
 
     //Nell'array aggiungo .length per evitare un ciclo infinito il ciclo in questione deve essere inizializzato solo una volta che la length dell'array è diversa da zero
-    if(array.length != 0){
-        for (let i = 0 ; i < array.length; i++) {
+    if (array.length != 0) {
+        for (let i = 0; i < array.length; i++) {
             //Variabile Val con valore identico al valore corrisposto nella posizione indicata dall'indice
             let val = array[i];
             console.log(val)
-    
+
             //Controllo se l'elemento di elems non è presente nell'oggetto
             if (!elems[val]) {
                 elems[val] = 1;
@@ -66,7 +66,7 @@ input.addEventListener('keypress', function(event) {
             console.log(array)
 
             //Secondo ciclo in lettura che incrementa (forse evitabile con il counter dentro l'oggetto)
-            for (let j = i; j < array.length; j++) {
+            /* for (let j = i; j < array.length; j++) {
                 //L'elemento nell'array è uguale all'elemento nel secondo array?
                 if(array[i] === array[j]) {
                     //incrementa il counter
@@ -78,11 +78,25 @@ input.addEventListener('keypress', function(event) {
                 }
             }
             //Rinizializza il counter
-            counter = 0;
+            counter = 0; */
         }
     }
-    
+
     console.log(elems)
+    //Inizializzo un contatore che mi conta quante volte è stato ripetuto il nome e prende il suo valore dall'oggetto, che si aggiorna ogni qualvolta inseriamo un valore nella lista, quando quel valore nella lista supera il valore precedentemente più scritto ne prende il posto e il contatore semplicemente continua ad incrementarsi.
+    let contatoreParola = 0;
+    let nomeRipetuto = "";
+    //Faccio un ciclo in elems per provare a salvare le sue proprietà
+    for (let key in elems) {
+        //Noto che da questo log in elems[key] sono presenti dei valori numerici che stanno ad indicare il numero di volte in cui una voce dell'oggetto è ripetuta
+        console.log(elems[key])
+        const counterInterno = elems[key];
+        //Faccio il controllo se il counter è maggiore del contatore allora il contatore prenderà il valore del counter il nomeRipetuto prenderà il valore della key
+        if (counterInterno > contatoreParola) {
+            contatoreParola = counterInterno;
+            nomeRipetuto = key;
+        }
+    }
 
     const alert = document.querySelector('.alert');
     console.log(alert)
@@ -91,9 +105,9 @@ input.addEventListener('keypress', function(event) {
     alert.classList.add('d-flex');
     //Il primo elemento che viene scritto nella lista è quello che sarà confrontato con gli altri dopo
     //Nel primo span metto quindi array[0] ossia item
-    alert.querySelector('span:first-child').innerText = item;
+    alert.querySelector('span:first-child').innerText = nomeRipetuto;
     //Nel secondo il counter
-    alert.querySelector('span:last-child').innerText = max;
+    alert.querySelector('span:last-child').innerText = contatoreParola;
 
-    console.log(`${item} è stato trovato : ${max} volte`);
+    console.log(`${nomeRipetuto} è stato trovato : ${contatoreParola} volte`);
 });
